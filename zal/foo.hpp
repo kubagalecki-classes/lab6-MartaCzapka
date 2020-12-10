d47561527e00882f1df3a52225327ed92c4203e5
MartaCzapka <01123769@pw.edu.pl>
2020-12-10 21:27:33 +0000

    2

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,9 +10,9 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    for_each(people.rbegin(), people.rend(), [&](auto& it) { return it.birthday(); });
-    transform(people.rbegin(), people.rend(), back_inserter(ret_v), [&](auto& it) {
-        return (it.isMonster() ? 'n' : 'y');
+    for_each(people.begin(), people.end(), [&](auto& it) { return it.birthday(); });
+    transform(people.cbegin(), people.cend(), ret_v.rbegin(), [](const Human& h) {
+        return h.isMonster() ? 'n' : 'y';
     });
 
     return ret_v;
