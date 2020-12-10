dcad66ae3583ca904c76b5088aa069ae8f48add0
MartaCzapka <01123769@pw.edu.pl>
2020-12-10 12:20:26 +0000

    zad7

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,22 @@
+#include "make_random_vector.hpp"
 #include <iostream>
-
+#include <vector>
+using namespace std;
+void print(vector< int > wek)
+{
+    for (int i = 0; i < 10; i++) {
+        cout << wek[i] << " ";
+    }
+    cout << "\n";
+}
 int main()
 {
-    puts("Hello, World!");
+    vector< int > wek;
+    wek = make_random_vector(10, 0, 10);
+    print(wek);
+    sort(wek.begin(), wek.end(), [](int a, int b) { return a > b; });
+    print(wek);
+    int j;
+    cin >> j;
+    cout << count_if(wek.begin(), wek.end(), [&](int i) { return i > j; });
 }
