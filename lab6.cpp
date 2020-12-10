4456e8e374bfaf5298023d18961df259468d8e3b
MartaCzapka <01123769@pw.edu.pl>
2020-12-10 21:18:47 +0000

    1

--- lab6.cpp
+++ lab6.cpp
@@ -13,23 +13,24 @@ void print(vector< int > wek)
 }
 int main()
 {
-    // vector< int > wek;
-    // wek = make_random_vector(10, 0, 10);
-    // print(wek);
-    // sort(wek.begin(), wek.end(), [](int a, int b) { return a > b; });
-    // print(wek);
-    // int j;
-    // cin >> j;
-    // cout << count_if(wek.begin(), wek.end(), [&](int i) { return i > j; });
-    string ciag;
-    cin >> ciag;
-    cout << ciag;
-    string s   = "piesek";
-    string k   = "kotek";
-    auto   il  = search(ciag.begin(), ciag.end(), s.begin(), s.end());
-    auto   il2 = search(ciag.begin(), ciag.end(), k.begin(), k.end());
-    if (il == ciag.end() && il2 == ciag.end())
-        return 0;
-    else
-        return 1;
+     vector< int > wek;
+     wek = make_random_vector(10, 0, 10);
+     print(wek);
+     find
+     sort(wek.begin(), wek.end(), [](int a, int b) { return a > b; });
+     print(wek);
+     int j;
+     cin >> j;
+     cout << count_if(wek.begin(), wek.end(), [&](int i) { return i > j; });
+    //string ciag;
+   // cin >> ciag;
+   // cout << ciag;
+   // string s   = "piesek";
+  //  string k   = "kotek";
+  //  auto   il  = search(ciag.begin(), ciag.end(), s.begin(), s.end());
+   // auto   il2 = search(ciag.begin(), ciag.end(), k.begin(), k.end());
+   // if (il == ciag.end() && il2 == ciag.end())
+    //    return 0;
+  //  else
+  //      return 1;
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -5,12 +5,15 @@
 #include <algorithm>
 #include <list>
 #include <vector>
-
+using namespace std;
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+    for_each(people.rbegin(), people.rend(), [&](auto& it) { return it.birthday(); });
+    transform(people.rbegin(), people.rend(), back_inserter(ret_v), [&](auto& it) {
+        return (it.isMonster() ? 'n' : 'y');
+    });
 
     return ret_v;
 }
