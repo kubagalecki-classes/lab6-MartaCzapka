4de10298970f997e7e547c3429cee6529a3a1529
MartaCzapka <01123769@pw.edu.pl>
2020-12-10 12:32:32 +0000

    cw 1

--- lab6.cpp
+++ lab6.cpp
@@ -1,5 +1,6 @@
 #include "make_random_vector.hpp"
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 void print(vector< int > wek)
@@ -11,12 +12,20 @@ void print(vector< int > wek)
 }
 int main()
 {
-    vector< int > wek;
-    wek = make_random_vector(10, 0, 10);
-    print(wek);
-    sort(wek.begin(), wek.end(), [](int a, int b) { return a > b; });
-    print(wek);
-    int j;
-    cin >> j;
-    cout << count_if(wek.begin(), wek.end(), [&](int i) { return i > j; });
+    // vector< int > wek;
+    // wek = make_random_vector(10, 0, 10);
+    // print(wek);
+    // sort(wek.begin(), wek.end(), [](int a, int b) { return a > b; });
+    // print(wek);
+    // int j;
+    // cin >> j;
+    // cout << count_if(wek.begin(), wek.end(), [&](int i) { return i > j; });
+    string ciag;
+    cin >> ciag;
+    cout << ciag;
+    auto il = adjacent_find(ciag.begin(), ciag.end());
+    if (il == ciag.end())
+        return 0;
+    else
+        return 1;
 }
